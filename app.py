f838e923deeddb81663bced480f8f2bf7757001c
Zorex <zorexsalvo@gmail.com>
2024-10-26 11:28:14 +0700

    Make content dynamic

--- app.py
+++ app.py
@@ -6,24 +6,27 @@ from textual.app import App, ComposeResult
 from textual.screen import Screen
 from textual.widgets import Static
 
+directory: str = "slides"
 
-def generate_slide(file):
+def generate_slide(file: str):
+    content = ""
+    with open(f"{directory}/{file}") as f:
+        content = f.read()
 
     class Slide(Screen):
         def compose(self) -> ComposeResult:
-            yield Static(file)
+            yield Static(content)
+
     return Slide
 
 
 class HeyIMadeThisWithPython(App):
     slide_number = 0
-    directory: str = "slides"
     SCREENS = {}
 
-    # or file in range() os.listdir(directory):
-    for file in range(10):
-        slide = generate_slide(str(file))
-        SCREENS[str(file)] = slide
+    for file in os.listdir(directory):
+        slide = generate_slide(file)
+        SCREENS[file] = slide
 
     def on_key(self, event: events.Key) -> None:
         screen_list = list(self.SCREENS.keys())
